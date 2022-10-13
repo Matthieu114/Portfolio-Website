3dc290c1c596609eb0141d64a5cae9c99ffac0a5
MatthieuDenis <mdenis300@gmail.com>
2022-10-13 23:36:20 +0200

    fix package json error

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         }}
       />
       <Routes>
-        <Route path='/' element={<MainPage />} />
+        <Route path='/portfolio-website' element={<MainPage />} />
       </Routes>
     </div>
   );
