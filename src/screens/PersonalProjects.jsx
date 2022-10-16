ccb8f9f932d4b5d745a85d0a8e420e67fa9f41f9
MatthieuDenis <mdenis300@gmail.com>
2022-10-16 13:06:47 +0200

    added project filtering / made projects always same size

--- src/screens/PersonalProjects.jsx
+++ src/screens/PersonalProjects.jsx
@@ -1,8 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Project from '../components/Project';
 import { projects } from '../data/projects';
 
-const PersonalProjects = ({ fullpageApi }) => {
+const PersonalProjects = () => {
+  const [projectsList, setProjectsList] = useState([]);
+  const [selectedCategory, setCategory] = useState();
+  const [activeItem, setActiveItem] = useState({ activeItemId: 1 });
+  let filteredList = useMemo(getFilteredList, [selectedCategory, projectsList]);
+
+  useEffect(() => {
+    setProjectsList(projects);
+  }, []);
+
+  const handleCategoryChange = (category) => {
+    setCategory(category);
+  };
+
+  const setActiveTarget = (event) => {
+    setActiveItem(event.currentTarget.id);
+    event.currentTarget.classList.toggle('active');
+  };
+
+  function getFilteredList() {
+    if (!selectedCategory) return projectsList;
+
+    return projectsList.filter((project) => project.category === selectedCategory);
+  }
+
   return (
     <div class='root-container' id='my-work'>
       <div class='projects-title-container'>
@@ -14,42 +38,72 @@ const PersonalProjects = ({ fullpageApi }) => {
           <ul class='filter-projects-list'>
             <li class='list-item'>Filter By</li>
             <li class='list-item'>
-              <a href='#' class='list-link active'>
+              <a
+                class='list-link'
+                data-list-item='all'
+                onClick={(e) => {
+                  handleCategoryChange(undefined);
+                  setActiveTarget(e);
+                }}>
                 All
               </a>
               &nbsp;
               {'\t/'}
             </li>
             <li class='list-item'>
-              <a href='#' class='list-link'>
+              <a
+                class='list-link'
+                data-list-item='web'
+                onClick={(e) => {
+                  handleCategoryChange('web');
+                  setActiveTarget(e);
+                }}>
                 Web Apps
               </a>
               &nbsp;
               {'\t/'}
             </li>
             <li class='list-item'>
-              <a href='#' class='list-link'>
+              <a
+                class='list-link'
+                data-list-item='data'
+                onClick={(e) => {
+                  handleCategoryChange('data');
+                  setActiveTarget(e);
+                }}>
                 Data science
               </a>
               &nbsp;
               {'\t/'}
             </li>
             <li class='list-item'>
-              <a href='#' class='list-link'>
+              <a
+                class='list-link'
+                data-list-item='game'
+                onClick={(e) => {
+                  handleCategoryChange('game');
+                  setActiveTarget(e);
+                }}>
                 Games
               </a>
               &nbsp;
               {'\t/'}
             </li>
             <li class='list-item'>
-              <a href='#' class='list-link'>
+              <a
+                class='list-link'
+                data-list-item='mobile'
+                onClick={(e) => {
+                  handleCategoryChange('mobile');
+                  setActiveTarget(e);
+                }}>
                 Mobile Apps
               </a>
             </li>
           </ul>
         </div>
-        <div class='projects-container'>
-          {projects.map((project) => {
+        <div class='projects-container' id='project-container'>
+          {filteredList.map((project) => {
             return <Project img={project.img} desc={project.description} path={project.path} name={project.name} />;
           })}
         </div>
